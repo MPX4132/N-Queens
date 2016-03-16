9255d28fef0ef59597a916b14fc5040114616660
Matias Barcenas <matias.barcenas@gmail.com>
2016-03-16 11:14:23 -0500

    Initial Commit

---
+++ N-Queens/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  N-Queens
+//
+//  Created by Matias Barcenas on 3/16/16.
+//  Copyright © 2016 Matias Barcenas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
